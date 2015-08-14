e8754b69065f035698609aaf8223ca1cd65e65c3
Jeffrey Wan <Jwan622@gmail.com>
2015-08-14 13:18:23 +0100

    pushing up to andela

--- lib/scrabble.rb
+++ lib/scrabble.rb
@@ -1,5 +1,50 @@
 class Scrabble
-  def self.score(word)
-    0
+  def self.values
+  {
+    "A"=>1, "B"=>3, "C"=>3, "D"=>2,
+    "E"=>1, "F"=>4, "G"=>2, "H"=>4,
+    "I"=>1, "J"=>8, "K"=>5, "L"=>1,
+    "M"=>3, "N"=>1, "O"=>1, "P"=>3,
+    "Q"=>10, "R"=>1, "S"=>1, "T"=>1,
+    "U"=>1, "V"=>4, "W"=>4, "X"=>8,
+    "Y"=>4, "Z"=>10
+  }
   end
 end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#
+# def self.score(word)
+#   word.to_s.upcase.chars.reduce(0) {|sum, letter| sum + values[letter] }
+# end
+#
+# def self.score_word(word, multiplier)
+#   numbers_array(word).zip(multiplier).map {|pair| pair.reduce(:*)}.reduce(:+)
+#
+# end
+#
+# def self.numbers_array(word)
+#   array_word(word).map { |letter| values[letter] }
+# end
+#
+# def self.array_word(word)
+#   word.upcase.chars
+# end
--- test/scrabble_test.rb
+++ test/scrabble_test.rb
@@ -5,8 +5,36 @@ require 'minitest/pride'
 
 class ScrabbleTest < Minitest::Test
   def test_it_can_score_a_single_letter
-    skip
     assert_equal 1, Scrabble.score("a")
     assert_equal 4, Scrabble.score("f")
+  end
+
+  def test_it_can_score_a_word
+    assert_equal 8, Scrabble.score("hello")
+  end
+
+  def test_score_method_can_handle_nil
+    skip
+    assert 0, Scrabble.score(nil)
+  end
+
+  def test_score_method_can_handle_empty_string
+    skip
+    assert 0, Scrabble.score("")
+  end
+
+  def test_score_word_method_returns_back_multiplied_value
+    skip
+    assert_equal 9, Scrabble.score_word("hello", [1,2,1,1,1])
+  end
+
+  def test_numbers_array_actually_returns_me_back_array_of_numbers
+    skip
+    assert_equal [4,1,1,1,1], Scrabble.numbers_array("hello")
+  end
+
+  def test_array_word_returns_back_array_upcase_numbers
+    skip
+    assert_equal ["H", "E", "L", "L", "O"], Scrabble.array_word("hello")
   end
 end
